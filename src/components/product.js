3b5811f8138c06a24408bfe543dc2ea8e8366c4c
anemcleod <58801411+anemcleod@users.noreply.github.com>
2021-04-06 12:47:47 +0200

    dynamic product info

--- src/components/product.js
+++ src/components/product.js
@@ -35,13 +35,66 @@ const Product = ({product}) => {
                         </div>
 
                         <div className="product-reviews">
-                        <div>{product.reviews.rating}</div>
-                        <div>{product.reviews.count} reviews</div>
-                        <div>{product.reviews.total_buyers} buyers</div>
+                            <div>{product.reviews.rating}</div>
+                            <div>{product.reviews.count} reviews</div>
+                            <div>{product.reviews.total_buyers} buyers</div>
+                        </div>
+
+                        <div className="product-reviews">
+                            <div>dynamic new price range</div>
+                            <div>/options <span>{Object.keys(product.options).length} Options</span> (Min.Order)</div>    
+                            <div>dynamic old price range</div>
+                        </div>
+
+                        <div className="shipping-offers">
+                            <div>icon</div>
+                            <div>Free shipping (up to $40)</div>
+                            <div>On-time delivery guaranteed</div>
+                            <div>icon</div>
+                        </div>
+
+                        <div className="product-discount">
+                            <div><span>{product.discount.amount} OFF</span></div>
+                            <div>Discount ends in icon {product.discount.end_date}</div>
+                        </div>
+
+                        <div className="product-options">
+                            <div>Options</div>
+                            <div>
+                                {
+                                    Object.keys(product.options).map((option, index) => {
+                                        return (
+                                            <div className="product-option" key={index}>
+                                                <div>{product.options[option].label}</div>
+                                                <div>{`${product.options[option].price.currency.symbol} ${product.options[option].price.value}`}</div>
+                                                <div>QTY</div>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+
+                            <div className="vendor-details">
+                                <div><span>icon</span><span>Trade Assurance</span> protects your Alibaba.com orders</div>
+                                <div>Payments: <span>icon</span><span>icon</span><span>icon</span></div>
+                                <div>Alibaba.com Logistics</div>
+                                <div>Inspection Solutions</div>
+                            </div>
                         </div>
                 </div>
 
                 <div className="product-purchase-container">
+                    <div className="shipping-method">
+                        <div>Ship to <span>{product.shipping.method.country}</span> by <span>{product.shipping.method.title}</span></div>
+                        <div>{`${product.shipping.method.cost.currency.symbol} ${product.shipping.method.cost.value}`}</div>
+                    </div>  
+                    <div className="shipping-lead">{`Lead time ${product.shipping.lead_time.value} icon`}</div>
+                    <div className="shipping-time">{`Shipping time ${product.shipping.method.shipping_time.value} icon`}</div>
+
+                    <div className="buttons-container">
+                        <button>Login to Purchase</button>
+                        <button>icon Contact the Supplier</button>
+                    </div>
                 
                 </div>
         </div>
